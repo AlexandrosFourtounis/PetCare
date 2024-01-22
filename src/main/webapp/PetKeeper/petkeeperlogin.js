633e525e85c382536cac3ae2bdb9462d78766631
Alexandros <csd5031@csd.uoc.gr>
2024-01-22 20:31:43 +0200

    Add ChatGPTAPI class and update help.html and petkeeper.html

--- src/main/webapp/PetKeeper/petkeeperlogin.js
+++ src/main/webapp/PetKeeper/petkeeperlogin.js
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function () {
-    isLoggedIn();
-});
+//$(document).ready(function () {
+//    isLoggedIn();
+//});
 
 
 function isLoggedIn() {
