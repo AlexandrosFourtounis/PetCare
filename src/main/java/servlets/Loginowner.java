df8f1dba7f7e1b9ca67fe533286f3094130cbd2d
georgiats0 <geotsa001@gmail.com>
2024-01-22 18:35:18 +0200

    added login

---
+++ src/main/java/servlets/Loginowner.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import database.tables.EditPetOwnersTable;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import mainClasses.PetOwner;
+
+public class Loginowner extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            HttpSession session = request.getSession(true);
+
+            EditPetOwnersTable petowner = new EditPetOwnersTable();
+            PetOwner pk = new PetOwner();
+
+            if ((pk = petowner.databaseToPetOwners(username, password)) != null) {
+                session.setAttribute("loggedIn", username);
+                response.setStatus(200);
+                String json = petowner.petOwnerToJSON(pk);
+                response.getWriter().write(json);
+            } else {
+                response.setStatus(403);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loggedIn") != null) {
+            try {
+                response.setStatus(200);
+                EditPetOwnersTable petkeeper = new EditPetOwnersTable();
+                PetOwner pk = new PetOwner();
+                pk = petkeeper.databaseToPetOwnersUsername(session.getAttribute("loggedIn").toString());
+                response.getWriter().write(pk.getUsername());
+            } catch (SQLException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            response.setStatus(403);
+        }
+    }
+
+}
---
+++ src/main/webapp/META-INF/PetOwner/js/loginj.js
@@ -0,0 +1,71 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function () {
+    isLoggedIn();
+});
+
+
+function isLoggedIn() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+
+            $("#ajaxContent").html("Welcome again" + xhr.responseText);
+
+        } else if (xhr.status !== 200) {
+            $("#choices").load("buttons.html");
+        }
+    };
+    xhr.open('GET', '../../Login');
+    xhr.send();
+}
+
+function loginpPOST() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            window.location.href = "../petowner.html";
+            $("#ajaxContent").html("Successful Login");
+            const responseData = JSON.parse(xhr.responseText);
+            $('#ajaxContent').append(createTableFromJSON(responseData));
+        } else if (xhr.status !== 200) {
+            $("#error").html("Wrong Credentials");
+            ('Request failed. Returned status of ' + xhr.status);
+        }
+    };
+    var data = $('#loginForm').serialize();
+    xhr.open('POST', '../../Loginowner');
+    xhr.setRequestHeader
+            ('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(data);
+}
+function logout() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            window.location.href = "../../index.html";
+            $("#ajaxContent").html("Successful Logout");
+        } else if (xhr.status !== 200) {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+    };
+    xhr.open('POST', '../../Logout');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send();
+}
+
+function createTableFromJSON(data) {
+    var html = "<table><tr><th>Category</th><th>Value</th></tr>";
+    for (const x in data) {
+        var category = x;
+        var value = data[x];
+        html += "<tr><td>" + category + "</td><td>" + value + "</td></tr>";
+    }
+    html += "</table>";
+    return html;
+
+}
